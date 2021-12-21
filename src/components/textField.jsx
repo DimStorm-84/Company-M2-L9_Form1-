a74e9013875f942e7588b0d13e07999563f5757a
DimStorm-84 <81226815+DimStorm-84@users.noreply.github.com>
2021-12-22 00:58:44 +0300

    M2 L9 P14 _not OK

--- src/components/textField.jsx
+++ src/components/textField.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+    const (showPassword, setShowPassword) = useState(false)
+    const getInputClasses = () => {
+        return "form-control" + (error ? "is-invalid": "");
+    };
+    const toggleShowPassword = () => {
+        setShowPassword((prevState)=>!prevState)
+    };
     return (
         <div className="mb-4">
             <label htmlFor={name}> {label} </label>
+            <div className ="input-group has-validation">
             <input
-                type={type}
+                type={showPassword ? "text": type}
                 id={name}
                 name={name}
                 value={value}
                 onChange={onChange}
-                className="form-control is-invalid"
+                className={getInputClasses()}
             />
+            {type==="password" && (
+             <button className = "btn btn-outline-secondary" type="button" onClick={toggleShowPassword}>
+             <i className={"bi bi-eye"+(showPassword ? "-slash" : "")}></i>
+             </button> )}
+</div>
             {error && (
                 <div div className="invalid-feedback">
                     {error}
